1b8a36963918642f49d260b85f338060f1aa31a9
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-06-23 15:51:46 +0700

    pengulangan dari awal hingga rear (akhir elemen) jika front lebih kecil sama dengan rear maka akan mencetak data yg berada di dalam array (front)

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -86,4 +86,14 @@ public:
 			FRONT_position = 0;
 
 
-			
+			//iterasi dari awal hingga rear
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+
+			}
+			cout << endl;
+		}
+	}
+};
+
