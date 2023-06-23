b2e8fe9cd1886c6ec4fe74b2ededb8cad8290478
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-06-23 15:02:45 +0700

    mengecek apakah antriannya penuh jika front sama dengan 0 dan rear berada di akhir posisi (rear +1) itu berarti antriannnya penuh dan akan menampilkan pesan "Queue Overflows"

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -15,5 +15,14 @@ public:
 		cout << "Enter a Number :";
 		cin >> num;
 		cout << endl;
+
+		// cek apakah antrian penuh 
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue Overflow\n";
+			return;
+		}
+
+		
 	}
+	
 };
